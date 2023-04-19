879da87a69f24f964b60bbe6d688eae6c91f1c7c
daventsai <daventsai75@gmail.com>
2023-04-19 11:24:20 -0500

    logout working

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,14 @@
-
+import { useNavigate } from "react-router-dom";
 
 export default function Home(){
     
+    const nav = useNavigate('');
+
+    function logOut(){
+        localStorage.removeItem('token');
+        nav('/login');
+    }
+
     return(
         <div>
             <header>
@@ -9,7 +16,9 @@ export default function Home(){
                 <button>Home</button>
                 <button>Posts</button>
                 <button>Profile</button>
-                <button>Log Out</button>
+                <button onClick={()=>
+                    logOut()
+                }>Log Out</button>
             </header>
             <div>
                 <h1>Welcome in</h1>
