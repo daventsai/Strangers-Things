53915357917f189e842dc0e9e393acd9408d976e
daventsai <daventsai75@gmail.com>
2023-04-19 11:04:15 -0500

    homepage login redirect if not logged in is working

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,37 @@
 import { useState } from 'react';
-import {Routes,Route} from 'react-router-dom';
+import {Routes,Route,Navigate,Outlet} from 'react-router-dom';
 import './App.css';
 import RegisterForm from './components/RegisterForm';
 import LoginForm from './components/LoginForm';
 import Home from './components/Home';
 
+function ProtectedComponent(props){
+  if (props.token === null){
+    return(<div className='App'>
+        <Navigate to='/login'/>
+      </div>)
+  }
+  else{
+    return(
+      <Outlet/>
+    )
+  }
+}
+
+
 function App() {
   const [token, setToken] = useState(localStorage.getItem('token'));
+  
 
-  console.log('Token from App: ',token);
   return (
     <div className='App'>
       <Routes>
         <Route path='/login' element={<LoginForm setToken={setToken}/>}/>
         <Route path='/register' element={<RegisterForm setToken={setToken}/>}/>
-        <Route path='/' element={<Home/>}/>
+        
+        <Route element={<ProtectedComponent token={token}/>}>
+          <Route path='/' element={<Home/>}/>
+        </Route>
       </Routes>
     </div>
   )
